0dce993849549e6a6644a87d9c05057ba9f75148
Norbert Mieczkowski <mieczkowskinorbert@gmail.com>
2016-02-17 23:18:42 +0100

    Added cors headers support

--- libs/server.js
+++ libs/server.js
@@ -1,11 +1,14 @@
 'use strict';
 
 var express = require('express');
+var cors = require('cors');
 var logger = require('./logger.js');
 
 var server = express();
 
 server.enable('trust proxy');
+
+server.use(cors());
 
 server.use(function (req, res, next) {
   logger.log(req.path + ' request received from ' + req.ip + '. ' +
