1fe0918f7e5f6c6c69ac454db20c21e492eb55bd
Norbert Mieczkowski <mieczkowskinorbert@gmail.com>
2016-02-29 21:07:17 +0100

    Added public/ prefix to the user avatar path in database

--- models/User.js
+++ models/User.js
@@ -30,13 +30,13 @@ var User = database.define('user', {
     field: 'avatar',
     type: Sequelize.STRING,
     allowNull: false,
-    defaultValue: 'users/default/avatar.png'
+    defaultValue: 'public/users/default/avatar.png'
   },
   avatarThumbnail: {
     field: 'avatar_thumbnail',
     type: Sequelize.STRING,
     allowNull: false,
-    defaultValue: 'users/default/avatar_thumbnail.png'
+    defaultValue: 'public/users/default/avatar_thumbnail.png'
   }
 }, {
   timestamps: true,
