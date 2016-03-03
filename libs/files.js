774a73acdb60c32d800708fc84f7486c134065cf
Norbert Mieczkowski <mieczkowskinorbert@gmail.com>
2016-03-03 20:33:23 +0100

    Added filesystem function such as mkdir, mv and rm

--- libs/files.js
+++ libs/files.js
@@ -1,11 +1,14 @@
 'use strict';
 
+var fs = require('fs');
 var _ = require('lodash');
 var when = require('when');
 var nodefn = require('when/node');
 var parallel = require('when/parallel');
 var readChunk = nodefn.lift(require('read-chunk'));
 var fileType = require('file-type');
+var mkdirp = require('mkdirp');
+var rimraf = require('rimraf');
 
 /**
  * Validate specified files for the correct formats.
@@ -93,9 +96,37 @@ var optional = function (expectedFileType) {
   return [expectedFileType, 'optional'];
 };
 
+/**
+ * Create specified directory.
+ * @param {string} path - Path of the directory to create.
+ */
+var mkdir = function(path) {
+  return nodefn.lift(mkdirp)(path, {});
+};
+
+/**
+ * Move file to specified location.
+ * @param {string} currentFilePath - Current path of the file.
+ * @param {string} newFilePath - New path for the file.
+ */
+var mv = function(currentFilePath, newFilePath) {
+  return nodefn.lift(fs.rename)(currentFilePath, newFilePath);
+};
+
+/**
+ * Remove specified file or directory.
+ * @param {string} path - Path of the file or directory.
+ */
+var rm = function(path) {
+  return nodefn.lift(rimraf)(path, {});
+};
+
 module.exports.validate = validate;
 module.exports.JpgFile = JpgFile;
 module.exports.PngFile = PngFile;
 module.exports.BmpFile = BmpFile;
 module.exports.ImageFile = ImageFile;
 module.exports.optional = optional;
+module.exports.mkdir = mkdir;
+module.exports.mv = mv;
+module.exports.rm = rm;
